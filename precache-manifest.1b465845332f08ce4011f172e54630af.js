5c249756064a98de2dbf22dff7c58d37b40de370
Alexandru Besa <besaalex17@yahoo.com>
2020-07-26 21:02:07 +0300

    Updates

---
+++ precache-manifest.1b465845332f08ce4011f172e54630af.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf141df6a1f166b3ebed77032f954dde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90d51a7362e8e3caa130",
+    "url": "/static/css/main.689c4250.chunk.css"
+  },
+  {
+    "revision": "c65684075252dd7c4c39",
+    "url": "/static/js/2.b9ca574b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b9ca574b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90d51a7362e8e3caa130",
+    "url": "/static/js/main.6da197ff.chunk.js"
+  },
+  {
+    "revision": "5a5021d0364687fd15b0",
+    "url": "/static/js/runtime-main.bb7f2b33.js"
+  },
+  {
+    "revision": "aaa2f7b01b394b7af89c10be65f37600",
+    "url": "/static/media/avatar.aaa2f7b0.jpg"
+  },
+  {
+    "revision": "a2ccfca4b1c3b07c3eccd5f3dd5a7283",
+    "url": "/static/media/batman.a2ccfca4.JPG"
+  },
+  {
+    "revision": "eea92339d6416f540b9f36ab987e3f7f",
+    "url": "/static/media/bt.eea92339.jpg"
+  },
+  {
+    "revision": "18c70b97ac6548df0842d23c77e4c26f",
+    "url": "/static/media/comment-box.18c70b97.JPG"
+  },
+  {
+    "revision": "8830c80b249a30c9c20b29ebb80e7907",
+    "url": "/static/media/contact-form.8830c80b.JPG"
+  },
+  {
+    "revision": "c533843fbeb83854df4ee112007af88b",
+    "url": "/static/media/cookies.c533843f.JPG"
+  },
+  {
+    "revision": "d2f68fcfaddf7f1b728285cfb8cf2c13",
+    "url": "/static/media/family.d2f68fcf.jpg"
+  },
+  {
+    "revision": "7edb4a71176ee275c07d48b41960f1e5",
+    "url": "/static/media/game-app.7edb4a71.JPG"
+  },
+  {
+    "revision": "f3a813e6554a01ef918f6a02f6247c50",
+    "url": "/static/media/js.f3a813e6.png"
+  },
+  {
+    "revision": "e08e844a548adde5041d7a725b9a26d8",
+    "url": "/static/media/myCV.e08e844a.pdf"
+  },
+  {
+    "revision": "aaa2f7b01b394b7af89c10be65f37600",
+    "url": "/static/media/myPhoto.aaa2f7b0.jpg"
+  },
+  {
+    "revision": "f5d8a73639db3be87142f7ae9379b341",
+    "url": "/static/media/player-obstacle.f5d8a736.JPG"
+  },
+  {
+    "revision": "7475cde8e512fd7e3dfcf91ea1adc582",
+    "url": "/static/media/robo.7475cde8.JPG"
+  },
+  {
+    "revision": "aaf0dcd06774328edc7de3cb0a9c5b93",
+    "url": "/static/media/rock.aaf0dcd0.JPG"
+  }
+]);
