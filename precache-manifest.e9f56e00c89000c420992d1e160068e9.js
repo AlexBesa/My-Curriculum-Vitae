5f2b426d5198f18fe9fd274a3101120e3e258130
Alexandru Besa <besaalex17@yahoo.com>
2020-07-31 19:34:29 +0300

    Updates

---
+++ precache-manifest.e9f56e00c89000c420992d1e160068e9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a3c6e571912f6226375e364f19ffe8",
+    "url": "/MyResume/index.html"
+  },
+  {
+    "revision": "cdee4abae502898b1be4",
+    "url": "/MyResume/static/css/main.59f874cc.chunk.css"
+  },
+  {
+    "revision": "c65684075252dd7c4c39",
+    "url": "/MyResume/static/js/2.b9ca574b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyResume/static/js/2.b9ca574b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdee4abae502898b1be4",
+    "url": "/MyResume/static/js/main.11772bc5.chunk.js"
+  },
+  {
+    "revision": "b34e950eca970baae910",
+    "url": "/MyResume/static/js/runtime-main.58c46158.js"
+  },
+  {
+    "revision": "aaa2f7b01b394b7af89c10be65f37600",
+    "url": "/MyResume/static/media/avatar.aaa2f7b0.jpg"
+  },
+  {
+    "revision": "a2ccfca4b1c3b07c3eccd5f3dd5a7283",
+    "url": "/MyResume/static/media/batman.a2ccfca4.JPG"
+  },
+  {
+    "revision": "eea92339d6416f540b9f36ab987e3f7f",
+    "url": "/MyResume/static/media/bt.eea92339.jpg"
+  },
+  {
+    "revision": "18c70b97ac6548df0842d23c77e4c26f",
+    "url": "/MyResume/static/media/comment-box.18c70b97.JPG"
+  },
+  {
+    "revision": "8830c80b249a30c9c20b29ebb80e7907",
+    "url": "/MyResume/static/media/contact-form.8830c80b.JPG"
+  },
+  {
+    "revision": "c533843fbeb83854df4ee112007af88b",
+    "url": "/MyResume/static/media/cookies.c533843f.JPG"
+  },
+  {
+    "revision": "d2f68fcfaddf7f1b728285cfb8cf2c13",
+    "url": "/MyResume/static/media/family.d2f68fcf.jpg"
+  },
+  {
+    "revision": "7edb4a71176ee275c07d48b41960f1e5",
+    "url": "/MyResume/static/media/game-app.7edb4a71.JPG"
+  },
+  {
+    "revision": "f3a813e6554a01ef918f6a02f6247c50",
+    "url": "/MyResume/static/media/js.f3a813e6.png"
+  },
+  {
+    "revision": "e08e844a548adde5041d7a725b9a26d8",
+    "url": "/MyResume/static/media/myCV.e08e844a.pdf"
+  },
+  {
+    "revision": "cd8a230c8638dfeb9c38aaa88c49f049",
+    "url": "/MyResume/static/media/myPhoto2.cd8a230c.jpg"
+  },
+  {
+    "revision": "f5d8a73639db3be87142f7ae9379b341",
+    "url": "/MyResume/static/media/player-obstacle.f5d8a736.JPG"
+  },
+  {
+    "revision": "7475cde8e512fd7e3dfcf91ea1adc582",
+    "url": "/MyResume/static/media/robo.7475cde8.JPG"
+  },
+  {
+    "revision": "aaf0dcd06774328edc7de3cb0a9c5b93",
+    "url": "/MyResume/static/media/rock.aaf0dcd0.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.271e8676a65ac3b51236fdbde4eb3dec.js"
+  "/MyResume/precache-manifest.e9f56e00c89000c420992d1e160068e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MyResume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
